d8e5e479e531c43964b8de22cba45342972ea72a
moonite <moonite1209@qq.com>
2025-05-28 00:37:22 +0800

    lab1

--- src/mr/master.go
+++ src/mr/master.go
@@ -9,23 +9,40 @@ import (
 	"path/filepath"
 )
 
+type WorkerInfo struct {
+	id       int
+	status   string // idle working
+	is_alive bool
+}
+type MapTask struct {
+	filename string
+}
+type ReduceTask struct {
+	filename string
+}
+
 type Master struct {
 	// Your definitions here.
-
+	workers      map[int]WorkerInfo
+	map_tasks    []MapTask
+	reduce_tasks []ReduceTask
 }
 
 // Your code here -- RPC handlers for the worker to call.
-func (m *Master) MyExample(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
+func (m *Master) KeepAlive(args *KeepAliveArgs, reply *KeepAliveReply) error {
+	pid := args.pid
+	status := args.status
+	m.workers[pid] = WorkerInfo{id: pid, status: status, is_alive: true}
+	reply.ack = true
 	return nil
 }
 
-// an example RPC handler.
-//
-// the RPC argument and reply types are defined in rpc.go.
-func (m *Master) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
+func (m *Master) ReqireTask(args *ReqireTaskArgs, reply *ReqireTaskReply) error {
+
+}
+
+func (m *Master) CompleteTask(args *CompleteTaskArgs, reply *CompleteTaskReply) error {
+
 }
 
 // start a thread that listens for RPCs from worker.go
@@ -46,9 +63,10 @@ func (m *Master) server() {
 // if the entire job has finished.
 func (m *Master) Done() bool {
 	ret := false
-
 	// Your code here.
-
+	if len(m.map_tasks) == 0 && len(m.reduce_tasks) == 0 {
+		ret = true
+	}
 	return ret
 }
 
@@ -56,7 +74,7 @@ func (m *Master) Done() bool {
 // main/mrmaster.go calls this function.
 // nReduce is the number of reduce tasks to use.
 func MakeMaster(files []string, nReduce int) *Master {
-	var inputs []string
+	inputs := []string{}
 	for i := range files {
 		f, e := filepath.Glob(files[i])
 		if e != nil {
@@ -64,8 +82,11 @@ func MakeMaster(files []string, nReduce int) *Master {
 		}
 		inputs = append(inputs, f...)
 	}
-	var nMap = len(inputs)
-	m := Master{}
+	map_tasks := []MapTask{}
+	for i := range inputs {
+		map_tasks = append(map_tasks, MapTask{filename: inputs[i]})
+	}
+	m := Master{map_tasks: map_tasks}
 
 	// Your code here.
 
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,48 +1,41 @@
 package mr
 
-import "fmt"
-import "log"
-import "net/rpc"
-import "hash/fnv"
+import (
+	"fmt"
+	"hash/fnv"
+	"log"
+	"net/rpc"
+	"os"
+)
 
-
-//
 // Map functions return a slice of KeyValue.
-//
 type KeyValue struct {
 	Key   string
 	Value string
 }
 
-//
 // use ihash(key) % NReduce to choose the reduce
 // task number for each KeyValue emitted by Map.
-//
 func ihash(key string) int {
 	h := fnv.New32a()
 	h.Write([]byte(key))
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
-//
 // main/mrworker.go calls this function.
-//
-func Worker(mapf func(string, string) []KeyValue,
-	reducef func(string, []string) string) {
-
-	// Your worker implementation here.
-
-	// uncomment to send the Example RPC to the master.
-	// CallExample()
-
+func Worker(mapf func(string, string) []KeyValue, reducef func(string, []string) string) {
+	pid := os.Getpid()
+	status := "alive"
 }
 
-//
 // example function to show how to make an RPC call to the master.
 //
 // the RPC argument and reply types are defined in rpc.go.
-//
+func CallKeepAlive(pid int, status string) {
+	args := KeepAliveArgs{pid, status}
+	reply := KeepAliveReply{}
+	call("Master.KeepAlive", &args, &reply)
+}
 func CallExample() {
 
 	// declare an argument structure.
@@ -61,11 +54,9 @@ func CallExample() {
 	fmt.Printf("reply.Y %v\n", reply.Y)
 }
 
-//
 // send an RPC request to the master, wait for the response.
 // usually returns true.
 // returns false if something goes wrong.
-//
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
 	sockname := masterSock()
